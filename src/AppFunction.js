60fcaf1c928a573a2dc9a523fae5a8a57c371c7b
Amit Bothra <amit.bothra@outlook.com>
2019-07-04 18:14:14 -0500

    using multiple listeners in useEffect

--- src/AppFunction.js
+++ src/AppFunction.js
@@ -4,17 +4,44 @@ const App = () => {
 
   const [count, setCount] = useState(0);
   const [isOn, setIsOn] = useState(false);
+  const [mousePosition, setMousePosition] = useState({x: null,y: null});
+  const [status, setStatus] = useState(navigator.onLine)
 
   useEffect( () => {
     document.title = `You have clicked ${count} times`;
-  })
+    window.addEventListener('mousemove', handleMouseMove);
+    window.addEventListener('online', handleOnline)
+    window.addEventListener('offline', handleOffline)
+
+
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove)
+      window.addEventListener('online', handleOnline)
+      window.addEventListener('offline', handleOffline)
+    }
+  }, [count])
 
   const incrementCount = () => {
     setCount(prevCount => prevCount + 1)
   }
 
+  const handleMouseMove = event => {
+    setMousePosition({
+      x: event.pageX,
+      y: event.pageY
+    })
+  }
+
   const toggleLight = () => {
     setIsOn(prevIsOn => !prevIsOn)
+  }
+
+  const handleOnline = () => {
+    setStatus(true)
+  }
+
+  const handleOffline = () => {
+    setStatus(false)
   }
   return(
     <>
@@ -37,7 +64,16 @@ const App = () => {
           }}
           alt="Flashlight"
           onClick={toggleLight}
-        /> 
+        />
+
+        <h2>Mouse Position</h2>
+        {/* <p>X position: {mousePosition.x}</p> */}
+        {/* <p>Y position: {mousePosition.y}</p> */}
+        {JSON.stringify(mousePosition, null, 2)}
+        <br />
+
+        <h2>Network Status</h2>
+        <p>You are <strong>{status ? "online" : "offline"}</strong></p>
     </>
   )
 }
