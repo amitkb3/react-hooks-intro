ff00d55d4b7a93265087e27e383b5f7f931a699b
Amit Bothra <amit.bothra@outlook.com>
2019-07-04 14:52:09 -0500

    useState Hook Component

---
+++ src/AppClass.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  
+  state = {
+    count: 0
+  }
+
+  incrementCount = () => {
+    this.setState({
+      count: this.state.count + 1
+    })
+  }
+
+  render() {
+    return (
+      <div >
+        <button onClick={this.incrementCount}>
+          I was Clicked {this.state.count} times!
+        </button>
+      </div>
+    );
+  }  
+}
+
+export default App;
